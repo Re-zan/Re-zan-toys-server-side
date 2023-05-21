cd6f1900f325e1428b071e4d70234d3d7788f132
Re-zan <zannat881all@gmail.com>
2023-05-21 14:42:32 +0600

    locat set up done

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+//middle
+app.use(cors());
+app.use(express.json());
+
+//testing
+app.get("/", (req, res) => {
+  res.send("Let make a toy market place");
+});
+
+//conntection
+app.listen(port);
